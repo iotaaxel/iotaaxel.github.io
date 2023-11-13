df2359a7619ca9200a672fc670777fd6bd33bcc2
Axel Iota <iota.axel@gmail.com>
2023-11-12 19:04:04 -0500

    Update google map api calls

--- js/google-map.js
+++ js/google-map.js
@@ -1,62 +1,54 @@
-
 var google;
+let map;
+let marker;
+let geocoder;
 
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.73784637451172,-73.98844909667969);
-    // 39.399872
-    // -8.224454
-    
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+// See https://developers.google.com/maps/documentation/javascript/examples/geocoding-simple#maps_geocoding_simple-javascript
+function initMap() {
+  // Create the Google Map using out element and options defined below
+  // Get the HTML DOM element that will contain your map 
+  // We are using a div with id="map" seen below in the <body>
+  map = new google.maps.Map(document.getElementById("map"), {
+    // Set mapOptions
+    zoom: 7,
+    center: { lat: 40.69847032728747, lng: -73.9514422416687 },
+    mapTypeControl: false,
+    // How you would like to style the map. 
+    scrollwheel: false,
+    styles: [
+        {
+            "featureType": "administrative.country",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                },
+                {
+                    "hue": "#ff0000"
+                }
+            ]
+        }
+    ]
+  });
 
-        // The latitude and longitude to center the map (always required)
-        center: myLatlng,
-
-        // How you would like to style the map. 
-        scrollwheel: false,
-        styles: [
-            {
-                "featureType": "administrative.country",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "visibility": "simplified"
-                    },
-                    {
-                        "hue": "#ff0000"
-                    }
-                ]
-            }
-        ]
-    };
-
-    
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['228 Park Ave S, New York, NY 10003'];
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: '../images/loc.jpg'
-            });
-
-        });
-    }
-    
+  const address = '228 Park Ave S, New York, NY 10003';
+  geocoder.geocode( { 'address': address});
 }
-window.addEventListener('load', init);
+
+function geocode(request) {
+  geocoder
+    .geocode(request)
+    .then((result) => {
+      const { results } = result;
+
+      map.setCenter(results[0].geometry.location);
+      marker.setPosition(results[0].geometry.location);
+      marker.setMap(map);
+      return results;
+    })
+    .catch((e) => {
+      alert("Geocode was not successful for the following reason: " + e);
+    });
+}
+
+window.initMap = initMap;
